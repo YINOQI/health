75207b52a2cda9af37ddfcb2d82c6d7470b92869
YINOQI <1028@yinoqi.com>
2024-03-28 20:59:49 +0800

    提交

--- health_mobile/src/main/java/com/liusp/controller/ValidateCodeController.java
+++ health_mobile/src/main/java/com/liusp/controller/ValidateCodeController.java
@@ -1,5 +1,6 @@
 package com.liusp.controller;
 
+import com.aliyuncs.exceptions.ClientException;
 import com.liusp.constant.MessageConstant;
 import com.liusp.constant.RedisMessageConstant;
 import com.liusp.entity.Result;
@@ -48,5 +49,24 @@ public class ValidateCodeController {
         return new Result(true, MessageConstant.SEND_VALIDATECODE_SUCCESS);
     }
 
+    //手机快速登录时发送手机验证码
+    @RequestMapping("/send4Login.do")
+    public Result send4Login(String telephone){
+        Integer code = ValidateCodeUtils.generateValidateCode(6);//生成6位数字验证码
+        try {
+            //发送短信
+            SMSUtils.sendShortMessage(SMSUtils.VALIDATE_CODE,telephone,code.toString());
+        } catch (ClientException e) {
+            e.printStackTrace();
+            //验证码发送失败
+            return new Result(false, MessageConstant.SEND_VALIDATECODE_FAIL);
+        }
+        System.out.println("发送的手机验证码为：" + code);
+        //将生成的验证码缓存到redis
+        redisTemplate.opsForValue().set(
+                telephone+ RedisMessageConstant.SENDTYPE_LOGIN,code.toString(),5 * 60, TimeUnit.SECONDS);
+        //验证码发送成功
+        return new Result(true, MessageConstant.SEND_VALIDATECODE_SUCCESS);
+    }
     
 }
--- health_service_provider/src/main/java/com/liusp/service/impl/MemberServiceImpl.java
+++ health_service_provider/src/main/java/com/liusp/service/impl/MemberServiceImpl.java
@@ -29,7 +29,7 @@ public class MemberServiceImpl implements MemberService{
     public List<Integer> findMemberCountByMonth(List<String> month) {
         List<Integer> list = new ArrayList<>();
         for(String m : month){
-            m = m + ".31";//格式：2019.04.31
+            m = m + ".30";//格式：2019.04.31
             Integer count = memberDao.findMemberCountBeforeDate(m);
             list.add(count);
         }
